093a7befdf190e1927a22ed246890483b08ec491
techkle9-jpg <techkle9@gmail.com>
2025-10-17 09:17:30 +0530

    Implement addition with input and error handling
    Add basic addition functionality with error handling.

---
+++ Math.py
@@ -0,0 +1,8 @@
+try:
+ a= float(input("enter first number:"))
+ b= float(input("enter second number:"))
+
+print (f"addition:{a+b}")
+print (f"addition:{a+b}")
+Expect valueError
+print("enter a valid number")
