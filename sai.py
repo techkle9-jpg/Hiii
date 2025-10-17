73f8a3f5e41a8d875f0c67f608c12a45e7653237
techkle9-jpg <techkle9@gmail.com>
2025-10-17 09:30:11 +0530

    Correct addition and subtraction output formatting
    Fix indentation and correct exception handling.

---
+++ sai.py
@@ -0,0 +1,11 @@
+try:
+    a= float(input("enter first number:"))
+    b= float(input("enter second number:"))
+
+print(f"addition:{a+b}")
+print(f"substraction:{a-b}")
+
+Except ValueError:
+
+print("enter a valid number")
+
